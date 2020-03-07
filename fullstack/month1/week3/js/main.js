427e200b00239047c04f27f437755925095e1e37
Allen <alleneben@gmail.com>
2020-03-07 05:26:45 -0500

    Declaration of variables, arrays and objects

---
+++ fullstack/month1/week3/js/main.js
@@ -0,0 +1,17 @@
+// Variable Declaration
+var name = "Kojo"
+let age=20
+const height = 80
+
+name = "Kelvin"
+age = 78
+
+
+
+
+// Arrays
+var myarray = [4,5,6,8,"Allen", "Collins"]
+
+
+// Objects
+var myobject = {name:"Kelvin", age:20,height: 80}
